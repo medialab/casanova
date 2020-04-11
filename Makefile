a8ad83c3da604c344f187b073d6501b188bd06bc
Yomguithereal <guillaumeplique@gmail.com>
2020-04-11 17:35:12 +0200

    Porting ContiguousRangeSet over from minet

--- Makefile
+++ Makefile
@@ -20,7 +20,7 @@ clean:
 
 lint:
 	@echo Linting source code using pep8...
-	pycodestyle --ignore E501,E722,W504 $(SOURCE) test
+	pycodestyle --ignore E501,E722,E741,W504 $(SOURCE) test
 	@echo
 	@echo Searching for unused imports...
 	importchecker $(SOURCE) | grep -v __init__ || true
--- casanova/__init__.py
+++ casanova/__init__.py
@@ -2,10 +2,13 @@
 # Casanova Library Endpoint
 # =============================================================================
 #
+from casanova.contiguous_range_set import ContiguousRangeSet
+
 from casanova.enricher import (
     CasanovaEnricher as enricher,
     ThreadSafeCasanovaEnricher as threadsafe_enricher
 )
+
 from casanova.reader import (
     CasanovaReader as reader
 )
