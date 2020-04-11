502bd62b507688702f2d8ae8435ed67e59a24ac8
Yomguithereal <guillaumeplique@gmail.com>
2020-04-11 17:25:14 +0200

    ThreadSafeEnricher

--- casanova/__init__.py
+++ casanova/__init__.py
@@ -2,5 +2,10 @@
 # Casanova Library Endpoint
 # =============================================================================
 #
-from casanova.enricher import CasanovaEnricher as enricher
-from casanova.reader import CasanovaReader as reader
+from casanova.enricher import (
+    CasanovaEnricher as enricher,
+    ThreadSafeCasanovaEnricher as threadsafe_enricher
+)
+from casanova.reader import (
+    CasanovaReader as reader
+)
