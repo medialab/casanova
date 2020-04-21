a04e1c37c311d74e42d5999eea3e38bbf5abf811
Yomguithereal <guillaumeplique@gmail.com>
2020-04-21 22:37:46 +0200

    Backwards

---
+++ benchmark/backwards.py
@@ -0,0 +1,7 @@
+import csv
+from file_read_backwards import FileReadBackwards
+
+with FileReadBackwards('./test/resources/people.csv', encoding='utf-8') as f:
+    for line in csv.reader(f):
+        print(line)
+        break
