6d2f6f27c60b7f78bda82c46ce624d476b4c8c76
Yomguithereal <guillaumeplique@gmail.com>
2021-04-15 17:42:49 +0200

    Adding resumer.buffer test

---
+++ test/resumer_test.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# Casanova Resumer Unit Tests
+# =============================================================================
+from casanova.resuming import Resumer
+
+
+class TestResumer(object):
+    def test_buffer(self, tmpdir):
+        output_path = str(tmpdir.join('./resumer-test.csv'))
+
+        resumer = Resumer(output_path)
+
+        resumer.buffer.append(list(range(2)))
+
+        assert list(resumer) == [[0, 1]]
+        assert list(resumer) == []
