4d09ac456757dc765c133a7e9e9c50db144099f8
Yomguithereal <guillaumeplique@gmail.com>
2021-05-07 22:20:32 +0200

    Editing bench

--- benchmark/performance.py
+++ benchmark/performance.py
@@ -11,28 +11,6 @@ import casanova
 @click.option('--headers/--no-headers', default=True)
 @click.option('--skip-std/--no-skip-std', default=True)
 def bench(path, column, headers=True, skip_std=True):
-    # if not skip_std:
-    #     with Timer('csvmonkey'):
-    #         with open(path, 'rb') as f:
-    #             for row in csvmonkey.from_file(f, header=headers):
-    #                 a = row[column]
-
-    # with Timer('casanova_monkey: basic'):
-    #     with open(path, 'rb') as f:
-    #         reader = casanova_monkey.reader(f, no_headers=not headers)
-    #         pos = reader.pos[column]
-
-    #         for row in reader:
-    #             a = row[pos]
-
-    # with Timer('casanova_monkey: lazy'):
-    #     with open(path, 'rb') as f:
-    #         reader = casanova_monkey.reader(f, no_headers=not headers, lazy=True)
-    #         pos = reader.pos[column]
-
-    #         for row in reader:
-    #             a = row[pos]
-
     if not skip_std:
         with Timer('csv.reader'):
             with open(path) as f:
@@ -49,12 +27,12 @@ def bench(path, column, headers=True, skip_std=True):
         with open(path) as f:
             reader = casanova.reader(f, no_headers=not headers)
             for row in reader:
-                a = row[reader.pos[column]]
+                a = row[reader.headers[column]]
 
     with Timer('casanova.reader: cached pos'):
         with open(path) as f:
             reader = casanova.reader(f, no_headers=not headers)
-            pos = reader.pos[column]
+            pos = reader.headers[column]
 
             for row in reader:
                 a = row[pos]
