9c9fed96b2220bac7fbcf3d2766bcde046c43ce7
Yomguithereal <guillaumeplique@gmail.com>
2023-05-13 22:20:51 +0200

    Bump 1.11.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as f:
 
 setup(
     name="casanova",
-    version="1.10.0",
+    version="1.11.0",
     description="Specialized & performant CSV readers, writers and enrichers for python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
